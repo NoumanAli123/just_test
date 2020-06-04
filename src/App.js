7ec211aaddcc2fe61cbdf34b1b01c64acde6a630
nabeel0954 <nabeel.ahmad7585@gmail.com>
2020-06-04 21:38:20 +0500

    adding the from

--- src/App.js
+++ src/App.js
@@ -7,6 +7,13 @@ class App extends Component {
     return (
       <div className="App">
        <h1 className="App">Validation Form</h1>
+       <form>
+       <label for="fname">First name:</label>
+       <input type="text" id="fname" name="fname" value="John"/>
+       <label for="lname">Last name:</label>
+       <input type="text" id="lname" name="lname" value="Doe"/>
+       <input type="submit" value="Submit"/>
+       </form>
       </div>
     );
   }
